1f6da8de454d9bbb3e333213cda48e896bdd2240
RoordinkFrank <frank.roordink@live.nl>
2019-05-06 13:57:48 +0200

    InitialCommit

---
+++ src/main/java/nl/hu/sie/bep/friendspammer/MongoSaver.java
@@ -0,0 +1,63 @@
+package nl.hu.sie.bep.friendspammer;
+
+
+import java.net.UnknownHostException;
+import java.util.Arrays;
+
+import org.bson.Document;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientOptions;
+import com.mongodb.MongoClientURI;
+import com.mongodb.MongoCredential;
+import com.mongodb.MongoException;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+public class MongoSaver {
+	
+	public static boolean saveEmail(String to, String from, String subject, String text, Boolean html) {
+		//String userName = "spammer";
+		//String password = "hamspam";
+		//String database = "friendspammer";
+		
+		String userName = "spammer";
+		String password = "hamspam";
+		String database = "friendspammer";
+		
+		MongoCredential credential = MongoCredential.createCredential(userName, database, password.toCharArray());
+		
+		boolean success = true;
+		
+		MongoClientURI uri = new MongoClientURI("mongodb+srv://opdracht2admin:opdracht2frank@cluster0-725c7.mongodb.net/test?retryWrites=true");
+
+		
+	    
+	    //new ServerAddress("ds227939.mlab.com", 27939), credential, MongoClientOptions.builder().build()
+		try (MongoClient mongoClient = new MongoClient(uri) ) {
+			
+			MongoDatabase db = mongoClient.getDatabase( database );
+			
+			MongoCollection<Document> c = db.getCollection("email");
+			
+			Document  doc = new Document ("to", to)
+			        .append("from", from)
+			        .append("subject", subject)
+			        .append("text", text)
+			        .append("asHtml", html);
+			c.insertOne(doc);
+		} catch (MongoException mongoException) {
+			System.out.println("XXXXXXXXXXXXXXXXXX ERROR WHILE SAVING TO MONGO XXXXXXXXXXXXXXXXXXXXXXXXXX");
+			mongoException.printStackTrace();
+			success = false;
+		}
+		
+		return success;
+ 		
+	}
+
+}
