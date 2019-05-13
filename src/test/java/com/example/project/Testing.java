f3808ced8ba60793988967f6ff9fcfb69ef3d445
RoordinkFrank <frank.roordink@live.nl>
2019-05-13 14:39:18 +0200

    added failing test

---
+++ src/test/java/com/example/project/Testing.java
@@ -0,0 +1,12 @@
+package com.example.project;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Testing {
+
+  @Test
+  public void test_fail() {
+    fail("Let's test the failing unit test");
+  } 
+}
